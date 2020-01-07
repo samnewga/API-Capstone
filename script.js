a8878c79801ba7d3da15fe372f2cb194845dfd58
Samael Newgate <newgate90s@gmail.com>
2020-01-07 01:39:59 +0000

    search animation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function(){
-  $('#search').click(showResults)
+  $('.search-box-btn').click(showResults)
 }
 
 
 function showResults(){
-  let searchTerm = document.querySelector('input[name="searchTerm"]').value
+  let searchTerm = document.querySelector('input[class="search-box-input"]').value
 
   let url1 = wikipediaURL + searchTerm;
 
@@ -12,7 +12,7 @@ function showResults(){
 
   let outputYoutube = document.querySelector('#outputYoutube');
   
-  outputWiki.innerHTML = "<h2>Search Term " + searchTerm + "</h2>";
+  outputWiki.innerHTML = `<h2 class="dataOutput">Search Term "  ${searchTerm}  "</h2>`;
   outputYoutube.innerHTML = "<h2>Videos of " + searchTerm + "</h2>";
 
   fetch(url1)
@@ -43,55 +43,3 @@ function showResults(){
   })
 
 }
-
-
-
-/*
-  fetch(url1)
-  .then(response=>response.json())
-  .then(response=>{
-    const pages = response["query"]["pages"];
-    const result = Object.values(pages)[0];
-    console.log(result)
-    outputWikip.innerHTML += '<div class="panel"><div class="wrap"><div class="content">' + title + '</div><a href="https://youtu.be/' + videoID + '" target="_blank"><img src="' + thumb + '" alt="' + title + '"></a></div></div>'
-  })
-*/
-
-/*
-function ajaxJS(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            callback(JSON.parse(xhr.responseText))
-        }
-    }
-    xhr.open('GET', url, true)
-    xhr.send();
-}
-*/
-
-/*
-  ajaxJS(url2,function(response){
-
-    for(let x in response.items){
-          console.log(response.items[x])
-          let title = response.items[x].snippet.title;
-          let desc = response.items[x].snippet.description;
-          let thumb = response.items[x].snippet.thumbnails.default.url;
-          output.innerHTML += '<div class="dataOutput">'+title+'<br>'+desc+'<br>'+thumb+'</div>';
-    }
-    
-  })
-}
-*/
-
-/*
-ajaxJS(url1, function (response) {
-                console.log(response)
-                for (var x in response) {
-                    var holder = typeof response[x] == 'string' ? response[x] : response[x][0];
-                    outputWiki.innerHTML += '<div class="dataOutput">' + holder + '</div>';
-                }
-            })
-
-*/
