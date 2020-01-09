6b64542b42060b28fbecd6142b5a8039fdd90ca0
Samael Newgate <39318392+samnewga@users.noreply.github.com>
2020-01-09 12:21:56 -0700

    Update script.js
    Comments

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+// Runs the showResults function when an event is found on the search form
 window.onload = function(){
   $('.search-form').submit(showResults);
 }
 
-
+// Function that shows the results of the APIs
 function showResults(e) {
   e.preventDefault();
-
+  
+  // Gets the value of searchTerm
   let searchTerm = document.querySelector('input[class="search-box-input"]').value
-
+  
+  // Puts together the wikipedia endpoint with the search term
   let url1 = wikipediaURL + searchTerm;
 
+  // Get the first element of the document with the id of outputWiki
   let outputWiki = document.querySelector('#outputWiki');
 
+   // Get the first element of the document with the id of outputYoutube
   let outputYoutube = document.querySelector('#outputYoutube');
   
+  // Changes the HTML content of outputWiki with a string and search term
   outputWiki.innerHTML = `<h2 class="dataOutput">Search Term "${searchTerm}"</h2>`;
 
+  // Changes the HTML content of outputYoutube with a string and search term
   outputYoutube.innerHTML = `<h2 class="dataOutput">Videos of "${searchTerm}"</h2>`;
 
+  // Fetches a JSON response from the Wikipedia API
   fetch(url1)
   .then(response=>response.json())
   .then(response=>{
@@ -28,9 +36,10 @@ function showResults(e) {
     outputWiki.innerHTML += '<div class="dataOutput">' + result.extract + '</div>';
   })
 
-
+  // Combines the youtube end point url with the search term and the set maximum results
   let url2 = youtubeURL + searchTerm + maxResults;
 
+  // Fetches a JSON response from the Youtube API
   fetch(url2)
   .then(response=>response.json())
   .then(response=>{
@@ -52,6 +61,7 @@ function showResults(e) {
 
 }
 
+// Shortens the youtube video titles 
 function shortenTitle(title) {
   let newTitle = title.substring(0,35)
 
