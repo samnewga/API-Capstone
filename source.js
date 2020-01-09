0cc1bb00b341ae4713d07c05e43ffc6efc2c8cf3
Samael Newgate <39318392+samnewga@users.noreply.github.com>
2020-01-09 12:15:50 -0700

    Update source.js
    comments

--- source.js
+++ source.js
@@ -1,7 +1,11 @@
+// Youtube API key
 const apiKey = 'AIzaSyCUsiD4hbQy7GNtf6V2CCeGiw0zHbluJxs'
 
+// Wikipedia search enpoint url with origin to fix return error
 const wikipediaURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&origin=*&prop=extracts&exintro&explaintext&redirects=1&titles='
 
+// Youtube API end point
 const youtubeURL = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+ apiKey +'&q='
 
+// Youtube maximum search results
 const maxResults = '&maxResults=8'
